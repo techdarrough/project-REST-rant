867e9b319de2be04b83e78bb500ee41a508e92d6
Preston Darrough <techdarrough@gmail.com>
2022-03-12 19:24:17 -0800

    Adding and rendering a new view

---
+++ views/places/new.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Def from "../default"
+
+
+
+let newForm = () => (
+    <Def>
+        <main>
+            <h1>Add a new place</h1>
+        </main>
+    </Def>
+)
+
+module.exports = newForm
